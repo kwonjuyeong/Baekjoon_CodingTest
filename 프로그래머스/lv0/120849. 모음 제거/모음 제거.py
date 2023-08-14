5da036957f23d2c532f89742593267247f9f8551
Kwon Juyeong <57522230+kwonjuyeong@users.noreply.github.com>
2023-08-14 12:58:55 +0900

    [level 0] Title: 모음 제거, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.py
@@ -0,0 +1,7 @@
+def solution(my_string):
+    a = my_string.replace('a', "")
+    a = a.replace('e', "")
+    a = a.replace('i', "")
+    a = a.replace('o', "")
+    a = a.replace('u', "")
+    return a
