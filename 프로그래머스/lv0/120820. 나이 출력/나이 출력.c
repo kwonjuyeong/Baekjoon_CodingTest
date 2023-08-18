185dd842564d800a353a4322b0cfd91d4f8fcd9b
Kwon Juyeong <57522230+kwonjuyeong@users.noreply.github.com>
2023-08-18 17:47:19 +0900

    [level 0] Title: 나이 출력, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120820. 나이 출력/나이 출력.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int age) {
+    int answer = 0;
+    if(0< age <=120){
+        answer = 2023 - age;
+    }
+    return answer;
+}
+
