43fa1ab41d034f4f070bbd8ae1d68ad72d04efca
Kwon Juyeong <57522230+kwonjuyeong@users.noreply.github.com>
2023-09-14 10:44:41 +0900

    Update 문자열 계산하기.py

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,11 +1,9 @@
 def solution(my_string):
-    try:
-        # 문자열을 공백을 기준으로 나눠서 리스트에 저장합니다.
+    
         tokens = my_string.split()
-        # 초기값은 첫 번째 숫자로 설정합니다.
+    
         result = int(tokens[0])
 
-        # 순서대로 연산자와 숫자를 확인하면서 계산합니다.
         for i in range(1, len(tokens), 2):
             operator = tokens[i]
             operand = int(tokens[i + 1])
@@ -16,6 +14,3 @@ def solution(my_string):
                 result -= operand
 
         return result
-    except Exception as e:
-        print("에러 발생:", e)
-        return None
