78c8f9f8670dfea0c0150d7ce14a8566d086805c
Kwon Juyeong <57522230+kwonjuyeong@users.noreply.github.com>
2023-09-16 11:21:39 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
@@ -1,17 +1,15 @@
 def solution(balls, share):
     answer = 0
-    balls_parent = 1
-    share_child = 1
-    ballshare_child = 1
+    n, m, nm = 1, 1, 1
     
     for ball in range(balls):
-        balls_parent *= ball + 1
+        n *= ball + 1
     
     for shareC in range(share):
-        share_child *= shareC + 1
+        m *= shareC + 1
     
     for allthing in range(balls - share):
-        ballshare_child *= allthing +1
+        nm *= allthing +1
     
     
-    return balls_parent / (ballshare_child * share_child)
+    return n / (nm * m)
