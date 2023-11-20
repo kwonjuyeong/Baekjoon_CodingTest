61dfd1d88444e7e38701ee86aa8bbb354c95967b
Kwon Juyeong <57522230+kwonjuyeong@users.noreply.github.com>
2023-11-20 16:36:29 +0900

    [D3] Title: 공과 잡초, Time: 119 ms, Memory: 45,308 KB -BaekjoonHub

---
+++ SWEA/D3/14555. 공과 잡초/공과 잡초.py
@@ -0,0 +1,11 @@
+T = int(input())
+ 
+for test_case in range(1, T + 1):
+    s = input()
+    answer = 0
+    
+    answer += s.count('()')
+    answer += s.count('(|')
+    answer += s.count('|)')
+    
+    print(f'#{test_case} {answer}')
