9ba780ac5d6be53ff58bed84777a12b672ddd9f1
Kwon Juyeong <57522230+kwonjuyeong@users.noreply.github.com>
2023-12-09 11:04:38 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 944 ms, Memory: 31408 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -0,0 +1,29 @@
+memo = {}
+
+def w(a, b, c):
+    if (a, b, c) in memo:
+        return memo[(a, b, c)]
+
+    if a <= 0 or b <= 0 or c <= 0:
+        return 1
+    elif a > 20 or b > 20 or c > 20:
+        memo[(a, b, c)] = w(20, 20, 20)
+        return memo[(a, b, c)]
+    elif a < b and b < c:
+        memo[(a, b, c)] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+        return memo[(a, b, c)]
+    else:
+        memo[(a, b, c)] = (
+            w(a-1, b, c) +
+            w(a-1, b-1, c) +
+            w(a-1, b, c-1) -
+            w(a-1, b-1, c-1)
+        )
+        return memo[(a, b, c)]
+
+while True:
+    a, b, c = map(int, input().split())
+    if a == -1 and b == -1 and c == -1:
+        break
+    result = w(a, b, c)
+    print(f"w({a}, {b}, {c}) = {result}")
