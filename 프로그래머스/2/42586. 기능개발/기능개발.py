73e5792145af12f54f2101d43a46dbd900bd1049
Kwon Juyeong <57522230+kwonjuyeong@users.noreply.github.com>
2024-02-23 11:11:57 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+def solution(progresses, speeds):
+    answer = []
+    progresses = deque(progresses)
+    speeds = deque(speeds)
+
+    while progresses:
+        for i in range(len(progresses)):
+            print(progresses[i])
+            progresses[i] += speeds[i]
+            print(progresses[i])
+        count = 0
+
+        while progresses and progresses[0] >= 100:
+            progresses.popleft()
+            speeds.popleft()
+            count += 1
+
+        if count > 0:
+            answer.append(count)
+
+    return answer
